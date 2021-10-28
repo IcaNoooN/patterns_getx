002e7f1591bcde2b2dc1cc71828c9cf941091147
Ibrohimjonn <creatprogramm@gmail.com>
2021-10-27 23:02:50 -0700

    Initial commit

---
+++ lib/controllers/create_controller.dart
@@ -0,0 +1,32 @@
+import 'dart:math';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:pattern_getx/model/post_model.dart';
+import 'package:pattern_getx/services/http_service.dart';
+import 'home_controller.dart';
+
+
+class CreateController extends GetxController {
+  var isLoading = false.obs;
+  var titleTextEditingController = TextEditingController().obs;
+  var bodyTextEditingController = TextEditingController().obs;
+  final _homeController = Get.put(HomeController());
+
+  apiPostCreate(BuildContext context)  async {
+    isLoading(true);
+
+    Post post = Post(title: titleTextEditingController().text, body: bodyTextEditingController().text, userId: Random().nextInt(pow(2, 30) - 1));
+
+    var response = await Network.GET(Network.API_CREATE, Network.paramsCreate(post));
+
+    if (response != null) {
+      Navigator.of(context).pop();
+      _homeController.apiPostList();
+      titleTextEditingController().text = '';
+      bodyTextEditingController().text = '';
+
+    }
+
+    isLoading(false);
+  }
+}
---
+++ lib/controllers/update_controller.dart
@@ -0,0 +1,28 @@
+import 'dart:math';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:pattern_getx/model/post_model.dart';
+import 'package:pattern_getx/services/http_service.dart';
+import 'home_controller.dart';
+
+class UpdateController extends GetxController {
+  var isLoading = false.obs;
+  var titleTextEditingController = TextEditingController().obs;
+  var bodyTextEditingController = TextEditingController().obs;
+  final _homeController = Get.put(HomeController());
+
+  apiPostUpdate(BuildContext context) async {
+    isLoading(true);
+
+    Post post = Post(id: Random().nextInt(pow(2, 30) - 1),title: titleTextEditingController().text, body: bodyTextEditingController().text, userId: Random().nextInt(pow(2, 30) - 1));
+
+    var response = await Network.PUT(Network.API_UPDATE + '1', Network.paramsUpdate(post));
+
+    if (response != null) {
+      Navigator.of(context).pop();
+      _homeController.apiPostList();
+    }
+
+    isLoading(false);
+  }
+}
